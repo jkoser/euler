0d9331e63407a4b35667e3f78281c6f423e5b0a3
Justin Koser <justin@user-agent-tel.net>
2016-05-08 22:18:47 -0400

    Explore bit patterns of perfect squares and their sums.

--- work/P75.java
+++ work/P75.java
@@ -1,3 +1,7 @@
+import java.util.HashSet;
+import java.util.Set;
+
+import com.google.common.base.Strings;
 
 public class P75 {
 
@@ -33,8 +37,35 @@ public class P75 {
 		});
 	}
 
+	public static void analyzeSquares() {
+		int bits = 4;
+		int n = 1 << bits;
+		Set<Integer> squareEnds = new HashSet<>();
+		for (int i = 0; i < n; i++) {
+			int iEnd = i & (n - 1);
+			int iSquaredEnd = (i * i) & (n - 1);
+			System.out.printf("%s  %s\n", Strings.padStart(Integer.toBinaryString(iEnd), bits, '0'),
+					Strings.padStart(Integer.toBinaryString(iSquaredEnd), bits, '0'));
+			squareEnds.add(iSquaredEnd);
+		}
+		System.out.println("--");
+		for (int q : squareEnds) {
+			System.out.println(Strings.padStart(Integer.toBinaryString(q), bits, '0'));
+		}
+		System.out.println("--");
+		Set<Integer> sumEnds = new HashSet<>();
+		for (int q : squareEnds) {
+			for (int r : squareEnds) {
+				sumEnds.add((q + r) & (n - 1));
+			}
+		}
+		for (int s : sumEnds) {
+			System.out.println(Strings.padStart(Integer.toBinaryString(s), bits, '0'));
+		}
+	}
+
 	public static void main(String[] args) {
-		solve();
+		analyzeSquares();
 	}
 
 }
